5300007741b9c7f1dc7f81cc97c3d3dae1ab608f
gm_desktop <gmesda23@gmail.com>
2020-01-13 14:16:54 -0500

    deleted unused files/ started hooks

---
+++ src/components/TodoApp.js
@@ -0,0 +1,22 @@
+import React, { useReducer } from 'react';
+
+
+function appReducer(state, action) {
+    switch (key) {
+        case value: 
+
+        break;
+
+        default:
+            break;
+    }
+}
+
+export default function TodosApp() {
+    const [state, dispatch] = useReducer(appReducer, [])
+    return ( 
+        <div>
+            <h1> Todos App </h1>
+        </div>
+    )
+}
