6bd8a15bf9cb489d8c1f709eea6fd7de61fcbb62
gm_desktop <gmesda23@gmail.com>
2020-01-13 14:22:18 -0500

    action dispatch new todo using Date.now

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,10 +2,17 @@ import React, { useReducer } from 'react';
 
 
 function appReducer(state, action) {
-    switch (key) {
-        case value: 
-
-        break;
+    switch (action.type) {
+        case 'add': {
+            return [
+                ...state,
+                {
+                    id: Date.now(),
+                    test: '',
+                    completed: false,
+                }
+            ]
+        }
 
         default:
             break;
@@ -17,6 +24,10 @@ export default function TodosApp() {
     return ( 
         <div>
             <h1> Todos App </h1>
+            <button onClick={() => dispatch({ type:'add'})}> New Todo </button>
+            {state.map(item => (
+                <div key={item.id}>{item.id}</div>
+            ))}
         </div>
     )
 }
